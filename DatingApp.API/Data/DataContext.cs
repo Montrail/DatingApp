be3b8b9db4809a861933d22bfa1a74019f4e28c4
Montrail Cooper <Way2Automate@Montrails-MacBook-Pro.local>
2018-08-28 09:10:20 -0700

    Added User Entity and Renamed Project

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -6,7 +6,7 @@ namespace Dating.API.Data
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base (options){}
-
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
