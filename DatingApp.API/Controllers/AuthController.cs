a75b372a24ffca95804c1142e8f0d5fd43108a53
Montrail Cooper <Way2Automate@Montrails-MacBook-Pro.local>
2018-09-12 09:48:50 -0700

    added nav logged and logged out logic

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -28,8 +28,6 @@ namespace DatingApp.API.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
-            //validate request
-
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
             if (await _repo.UserExists(userForRegisterDto.Username))
@@ -50,7 +48,7 @@ namespace DatingApp.API.Controllers
         {
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
-            if (userForLoginDto == null)
+            if (userFromRepo == null)
                 return Unauthorized();
 
             var claims = new[]
